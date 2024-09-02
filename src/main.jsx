e8a122882592644a3297787a116b8f424b6a1ca1
Adithya Chandrashekar <addyac1@gmail.com>
2024-09-02 09:02:47 -0400

    modify to use hashrouter again

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import WaitlistLandingPage from './waitlistPage.jsx';
 import PrivacyPolicy from './PrivacyPolicy.jsx';
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//this is the deployment version
 export default defineConfig({
   plugins: [react()],
     build: {
     outDir: 'dist',
   },
   base: './'
-})
+});
+
+// This is the npm run dev version
+// export default defineConfig({
+
+// })
